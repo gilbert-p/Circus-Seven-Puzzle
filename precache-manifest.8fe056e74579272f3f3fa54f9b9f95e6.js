56d000d5827baac3d5736e39e7217507c0072471
epoxi117 <gplacid2@calstatela.edu>
2020-06-17 09:43:06 -0700

    Updates

---
+++ precache-manifest.8fe056e74579272f3f3fa54f9b9f95e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c5582517e7f5bd835680486cb70b0b7",
+    "url": "/Circus-Seven-Puzzle/index.html"
+  },
+  {
+    "revision": "080d8b5320c285f1fc39",
+    "url": "/Circus-Seven-Puzzle/static/css/main.27bed868.chunk.css"
+  },
+  {
+    "revision": "bdd41f267c806ecfc92f",
+    "url": "/Circus-Seven-Puzzle/static/js/2.fc075c5b.chunk.js"
+  },
+  {
+    "revision": "79780c1c5b94754b0976317971f3fe6b",
+    "url": "/Circus-Seven-Puzzle/static/js/2.fc075c5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "080d8b5320c285f1fc39",
+    "url": "/Circus-Seven-Puzzle/static/js/main.5e3d1235.chunk.js"
+  },
+  {
+    "revision": "d4ec0f0d68879ce8e922",
+    "url": "/Circus-Seven-Puzzle/static/js/runtime-main.afe4b528.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Circus-Seven-Puzzle/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a17848a3aed5f50aab2b335cfa15113a.js"
+  "/Circus-Seven-Puzzle/precache-manifest.8fe056e74579272f3f3fa54f9b9f95e6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Circus-Seven-Puzzle/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
