96c6a85d3a5b57923f2559b9b3c39f4e85afbe63
epoxi117 <gplacid2@calstatela.edu>
2020-06-17 09:40:45 -0700

    Updates

---
+++ precache-manifest.a17848a3aed5f50aab2b335cfa15113a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a17490ee4b06e39d9b372267f3e0389d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa1ce90ba8cf269ed977",
+    "url": "/static/css/main.27bed868.chunk.css"
+  },
+  {
+    "revision": "bdd41f267c806ecfc92f",
+    "url": "/static/js/2.fc075c5b.chunk.js"
+  },
+  {
+    "revision": "79780c1c5b94754b0976317971f3fe6b",
+    "url": "/static/js/2.fc075c5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa1ce90ba8cf269ed977",
+    "url": "/static/js/main.0fb0e322.chunk.js"
+  },
+  {
+    "revision": "2af987b0bda9890f3009",
+    "url": "/static/js/runtime-main.76a6cd7f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
